9819495322fa82d521abe38643dacc434d9c2a8e
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-16 12:11:53 +0200

    -C# Advanced - Stack-And-Queues-Ex - 02

---
+++ C#-Advanced/Stacks-And-Queues/Exercise/Stacks-And-Queues-Ex/02-Basic-Queue-Operation/BasicQueueOperation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Basic_Queue_Operation
+{
+    class BasicQueueOperation
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var queue = new Queue<int>(tokens[0]);
+
+            for (int i = 0; i < tokens[0]; i++)
+            {
+                int elementToPush = elements[i];
+
+                queue.Enqueue(elementToPush);
+            }
+
+            for (int i = 0; i < tokens[1]; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(tokens[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                int smallestElement = 0;
+
+                if (queue.Count > 0)
+                {
+                    smallestElement = queue.Dequeue();
+
+                    while (queue.Count > 0)
+                    {
+                        int currElement = queue.Dequeue();
+                        if (currElement < smallestElement)
+                        {
+                            smallestElement = currElement;
+                        }
+                    }
+                }
+
+                Console.WriteLine(smallestElement);
+            }
+        }
+    }
+}
