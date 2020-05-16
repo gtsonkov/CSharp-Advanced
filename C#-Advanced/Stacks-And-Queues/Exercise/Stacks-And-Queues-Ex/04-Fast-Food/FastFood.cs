0de3031bf3da79417575e21fd0e12bf3b825a5df
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-16 14:23:21 +0200

    -C# Advanced - Stack-And-Queues-Ex - 04, 05

---
+++ C#-Advanced/Stacks-And-Queues/Exercise/Stacks-And-Queues-Ex/04-Fast-Food/FastFood.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04_Fast_Food
+{
+    class FastFood
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            var ordersInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> orders = new Queue<int>();
+
+            foreach (var order in ordersInput)
+            {
+                orders.Enqueue(order);
+            }
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count > 0)
+            {
+                if (orders.Peek() <= foodQuantity)
+                {
+                    int order = orders.Dequeue();
+                    foodQuantity -= order;
+                }
+                else
+                {
+                    Console.WriteLine("Orders left: " + String.Join(" ",orders));
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
---
+++ C#-Advanced/Stacks-And-Queues/Exercise/Stacks-And-Queues-Ex/05-Fashion-Boutique/FashionBotique.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Fashion_Boutique
+{
+    class FashionBotique
+    {
+        static void Main(string[] args)
+        {
+            int[] boxInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+            Stack<int> box = new Stack<int>(boxInput.Count());
+
+            foreach (var item in boxInput)
+            {
+                box.Push(item);
+            }
+
+            int currRackState = 0;
+            int racksUsed = 1;
+
+            while (box.Count>0)
+            {
+                int currItem = box.Pop();
+                if (currRackState + currItem <= rackCapacity)
+                {
+                    currRackState += currItem;
+                }
+                else
+                {
+                    currRackState = currItem;
+                    racksUsed += 1;
+                }
+            }
+
+            Console.WriteLine(racksUsed);
+        }
+    }
+}
