e9a9194ec8987ace9b44ba375f11f6627409bdf5
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-19 20:36:30 +0200

    -Stacks and Queues Lab -05 and -06

---
+++ C#-Advanced/Stacks-And-Queues/Lab/Stacks-And-Queues/05-Print-Even-Numbers/PrintEvenNzmbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Print_Even_Numbers
+{
+    class PrintEvenNzmbers
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(x=> x%2 == 0)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>(input);
+
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
---
+++ C#-Advanced/Stacks-And-Queues/Lab/Stacks-And-Queues/06-Surmarket/Supermarket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Supermarket
+{
+    class Supermarket
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+
+            Queue<string> names = new Queue<string>();
+            while (name != "End")
+            {
+                if (name == "Paid")
+                {
+                    while (names.Count > 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                }
+                else
+                {
+                    names.Enqueue(name);
+                }
+
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
