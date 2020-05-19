0bd2e9999560678a7391fdf0897cfa63d4a34e77
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-19 20:14:53 +0200

    -Stacks and Queues Lab -03 and -04

---
+++ C#-Advanced/Stacks-And-Queues/Lab/Stacks-And-Queues/03-Simple-Calculator/SimpleCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Simple_Calculator
+{
+    public class SimpleCalculator
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ");
+            Stack<string> stack = new Stack<string>(input.Reverse());
+            
+            while (stack.Count > 1)
+            {
+                int first = int.Parse(stack.Pop());
+                string opr = stack.Pop();
+                int second = int.Parse(stack.Pop());
+
+                switch (opr)
+                {
+                    case "+":
+                        stack.Push((first + second).ToString());
+                        break;
+
+                    case "-":
+                        stack.Push((first - second).ToString());
+                        break;
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ C#-Advanced/Stacks-And-Queues/Lab/Stacks-And-Queues/04-Matching-Brackets/MatchingBrackets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_Matching_Brackets
+{
+    class MatchingBrackets
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char value = input[i];
+
+                if (value == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (value == ')')
+                {
+                    int index = stack.Pop();
+                    string part = input.Substring(index, i - index + 1);
+
+                    Console.WriteLine(part);
+                }
+            }
+        }
+    }
+}
