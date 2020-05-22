99ef8f96ab16c5bc2deccdc2b45497b311db7c52
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-22 21:23:57 +0200

    -Multidimensional Arrays -Lab -06, -07

---
+++ C#-Advanced/Multidimensional-Arrays/Lab/Multidimensional-Arrays-Lab/06-Jagget-Array-Manipulation/JaggetArrazManipulation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_Jagget_Array_Manipulation
+{
+    class JaggetArrayManipulation
+    {
+        static void Main(string[] args)
+        {
+            int rowNumber = int.Parse(Console.ReadLine()); // Read row numbers
+
+            int[][] jaggetArr = new int[rowNumber][]; // Create Jagged Array
+
+            for (int i = 0; i < rowNumber; i++) //Fill Jaget array with user input
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                jaggetArr[i] = arr;
+            }
+
+            //Read and execute commands
+
+            string[] tokens = Console.ReadLine().Split().ToArray();
+
+            while (tokens[0] != "END")
+            {
+                string command = tokens[0];
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+                int value = int.Parse(tokens[3]);
+
+                if (AreValideCoordinates(row,col,jaggetArr))
+                {
+                    switch (command)
+                    {
+                        case "Add":
+
+                            jaggetArr[row][col] += value;
+
+                            break;
+
+                        case "Subtract":
+
+                            jaggetArr[row][col] -= value;
+
+                            break;
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+               
+
+                tokens = Console.ReadLine().Split().ToArray();
+            }
+
+            //Print Result
+            foreach (var arr in jaggetArr)
+            {
+                Console.WriteLine(string.Join(" ",arr));
+            }
+        }
+
+        private static bool AreValideCoordinates(int row, int col, int[][] jaggetArr)
+        {
+            return ((row >=0 ) 
+                && (row <= (jaggetArr.Length - 1)) 
+                && (col >= 0) 
+                && (col <= (jaggetArr[row].Length - 1)));
+        }
+    }
+}
---
+++ C#-Advanced/Multidimensional-Arrays/Lab/Multidimensional-Arrays-Lab/07-Pascal-Triangle/PascalTriangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07_Pascal_Triangle
+{
+    class PascalTriangle
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int currRow = 1;
+
+            long[][] triangle = new long[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                triangle[i] = new long[currRow];
+                triangle[i][0] = 1;
+                triangle[i][currRow - 1] = 1;
+
+                if (currRow > 2)
+                {
+                    for (int j = 1; j < currRow-1; j++)
+                    {
+                        triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                    }
+                }
+
+                currRow++;
+            }
+
+            foreach (var arr in triangle)
+            {
+                Console.WriteLine(string.Join(" ",arr));
+            }
+        }
+    }
+}
