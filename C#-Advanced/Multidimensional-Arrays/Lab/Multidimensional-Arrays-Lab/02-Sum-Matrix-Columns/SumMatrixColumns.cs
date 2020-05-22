cf55aaf3fbe5b0ae9673343e68698d74568422e5
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-22 19:06:31 +0200

    -Multidimensional Arrays -Lab -02

---
+++ C#-Advanced/Multidimensional-Arrays/Lab/Multidimensional-Arrays-Lab/02-Sum-Matrix-Columns/SumMatrixColumns.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_Sum_Matrix_Columns
+{
+    class SumMatrixColumns
+    {
+        static void Main(string[] args)
+        {
+            var dimensions = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+
+            int[] sumColumns = new int[dimensions[1]];
+
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                int[] NumbersInput = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < dimensions[1]; j++)
+                {
+                    matrix[i, j] = NumbersInput[j];
+                    sumColumns[j] += matrix[i, j];
+                }
+            }
+
+            foreach (var colSum in sumColumns)
+            {
+                Console.WriteLine(colSum);
+            }
+        }
+    }
+}
