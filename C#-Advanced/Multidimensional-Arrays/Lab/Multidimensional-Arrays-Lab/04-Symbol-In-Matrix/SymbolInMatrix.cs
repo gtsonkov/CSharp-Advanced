921fc3fdf5c001718fc1d7f1bbcc57188eee91fb
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-23 22:04:57 +0200

    -init -Multidimensional Arrays Lab -04

---
+++ C#-Advanced/Multidimensional-Arrays/Lab/Multidimensional-Arrays-Lab/04-Symbol-In-Matrix/SymbolInMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04_Symbol_In_Matrix
+{
+    class SymbolInMatrix
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+            char charToSearch = char.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i,j] == charToSearch)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{charToSearch} does not occur in the matrix");
+        }
+    }
+}
