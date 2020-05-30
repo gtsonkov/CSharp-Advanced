7a9fbf765730b8802d5ded00a5e7574ef2c914b9
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-30 16:39:28 +0200

    -Multidimensional Arrays Exercise -07, -09

---
+++ C#-Advanced/Multidimensional-Arrays/Exercise/Multidimensional-Arrays-Exercise/07-KnightGame/KnightGame.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace _07_KnightGame
+{
+    class KnightGame
+    {
+        struct Position
+        {
+            public byte row;
+            public byte col;
+            public Position(byte row, byte col)
+            {
+                this.row = row;
+                this.col = col;
+            }
+        }
+        static void Main(string[] args)
+        {
+            byte size = byte.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+            for (byte i = 0; i < size; i++)
+            {
+                string fillField = Console.ReadLine();
+                for (byte j = 0; j < size; j++)
+                {
+                    matrix[i, j] = fillField[j];
+                }
+            }
+            int removed = 0;
+            var CurrPos = new Position();
+            var MaxPos = new Position();
+            while (true)
+            {
+                byte maxAttack = 0;
+                byte attacks = 0;
+                for (byte i = 0; i < size; i++)
+                {
+                    CurrPos.row = i;
+                    for (byte j = 0; j < size; j++)
+                    {
+                        CurrPos.col = j;
+                        if (matrix[CurrPos.row, CurrPos.col] == 'K')
+                        {
+                            for (byte k = 0; k < 8; k++)
+                            {
+                                if (CheckConditions(matrix, CurrPos, size, k))
+                                {
+                                    attacks += 1;
+                                }
+                            }
+                            if (attacks > maxAttack)
+                            {
+                                maxAttack = attacks;
+                                MaxPos = CurrPos;
+                            }
+                            attacks = 0;
+                        }
+                    }
+                }
+                if (maxAttack > 0)
+                {
+                    matrix[MaxPos.row, MaxPos.col] = '0';
+                    removed++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(removed);
+            //Print Option
+            //for (byte i = 0; i < size; i++)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        Console.Write(matrix[i,j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+        private static bool CheckConditions(char[,] matrix, Position tempPos, byte size, byte index)
+        {
+            switch (index)
+            {
+                case 0:
+                    tempPos.row -= 1;
+                    tempPos.col -= 2;
+                    break;
+                case 1:
+                    tempPos.row += 1;
+                    tempPos.col -= 2;
+                    break;
+                case 2:
+                    tempPos.row -= 1;
+                    tempPos.col += 2;
+                    break;
+                case 3:
+                    tempPos.row += 1;
+                    tempPos.col += 2;
+                    break;
+                case 4:
+                    tempPos.row -= 2;
+                    tempPos.col -= 1;
+                    break;
+                case 5:
+                    tempPos.row -= 2;
+                    tempPos.col += 1;
+                    break;
+                case 6:
+                    tempPos.row += 2;
+                    tempPos.col -= 1;
+                    break;
+                case 7:
+                    tempPos.row += 2;
+                    tempPos.col += 1;
+                    break;
+            }
+            //Check InBoarder
+            if (tempPos.row >= 0
+            && tempPos.col >= 0
+            && tempPos.row < size
+            && tempPos.col < size)
+            {
+                //Chek if Attack
+                if (matrix[tempPos.row, tempPos.col] == 'K')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ C#-Advanced/Multidimensional-Arrays/Exercise/Multidimensional-Arrays-Exercise/09-Miner/Miner.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_Miner
+{
+    class Miner
+    {
+        struct Position
+        {
+            public int row;
+            public int col;
+            public Position(int row, int col)
+            {
+                this.row = row;
+                this.col = col;
+            }
+        }
+
+        static Position currPosition;
+        static string[,] field;
+        static int coalCount;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            field = new string[n, n];
+
+            coalCount = 0;
+
+            string[] inputCommands = Console.ReadLine().Split().ToArray();
+            Queue<string> commands = new Queue<string>(inputCommands);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+
+
+                for (int j = 0; j < n; j++)
+                {
+                    field[i, j] = input[j];
+
+                    if (input[j] == "s")
+                    {
+                        currPosition = new Position(i, j);
+                    }
+
+                    else if (input[j] == "c")
+                    {
+                        coalCount++;
+                    }
+                }
+            }
+
+            while (commands.Count > 0)
+            {
+                string currComand = commands.Dequeue();
+                switch (currComand)
+                {
+                    case "left":
+                        if (checkOnField(0, currPosition.col - 1))
+                        {
+                            currPosition.col -= 1;
+
+                            var currentGood = getCurrentGood();
+
+                            if (currentGood == "c")
+                            {
+                                coalCount--;
+
+                                field[currPosition.row, currPosition.col] = "*";
+                            }
+
+                            if (checkStatement(currentGood, coalCount))
+                            {
+                                return;
+                            }
+                        }
+
+                        break;
+
+                    case "right":
+                        if (checkOnField(0, currPosition.col + 1))
+                        {
+                            currPosition.col += 1;
+
+                            var currentGood = getCurrentGood();
+
+                            if (currentGood == "c")
+                            {
+                                coalCount--;
+
+                                field[currPosition.row, currPosition.col] = "*";
+                            }
+
+                            if (checkStatement(currentGood, coalCount))
+                            {
+                                return;
+                            }
+                        }
+
+                        break;
+
+                    case "up":
+                        if (checkOnField(currPosition.row - 1, 0))
+                        {
+                            currPosition.row -= 1;
+
+                            var currentGood = getCurrentGood();
+
+                            if (currentGood == "c")
+                            {
+                                coalCount--;
+
+                                field[currPosition.row, currPosition.col] = "*";
+                            }
+
+                            if (checkStatement(currentGood, coalCount))
+                            {
+                                return;
+                            }
+                        }
+
+                        break;
+
+                    case "down":
+                        if (checkOnField(currPosition.row + 1, 0))
+                        {
+                            currPosition.row += 1;
+
+                            var currentGood = getCurrentGood();
+
+                            if (currentGood == "c")
+                            {
+                                coalCount--;
+
+                                field[currPosition.row, currPosition.col] = "*";
+                            }
+
+                            if (checkStatement(currentGood, coalCount))
+                            {
+                                return;
+                            }
+                        }
+
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{coalCount} coals left. ({currPosition.row}, {currPosition.col})");
+        }
+
+        private static bool checkStatement(string currentGood, int coalCount)
+        {
+            if (coalCount == 0)
+            {
+                Console.WriteLine($"You collected all coals! ({currPosition.row}, {currPosition.col})");
+                return true;
+            }
+
+            else if (currentGood == "e")
+            {
+                Console.WriteLine($"Game over! ({currPosition.row}, {currPosition.col})");
+                return true;
+            }
+            return false;
+        }
+
+        private static string getCurrentGood()
+        {
+            return field[currPosition.row, currPosition.col];
+        }
+
+        private static bool checkOnField(int row, int col)
+        {
+            return ((row >= 0) && (row < field.GetLength(0)) && (col >= 0) && (col < field.GetLength(1)));
+        }
+    }
+}
