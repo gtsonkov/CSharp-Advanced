1815bd8833efd1004360fe1110b90c418d49ecd2
Georgi Tsonkov <gtsonkov@abv.bg>
2020-05-31 16:26:45 +0200

    -init Sets and Dictionaries -Ex -07, -08

---
+++ C#-Advanced/Sets-And-Dictionaries/Exercise/Sets-And-Dictionaries-Exercise/07-The-V-Logger/TheVLogger.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_The_V_Logger
+{
+    class TheVLogger
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<List<string>>> vLoggerList = new Dictionary<string, List<List<string>>>();
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            while (input[0] != "Statistics")
+            {
+                string userName = input[0]; 
+                string command = input[1];
+                if (command == "joined")
+                {
+                    if (!(vLoggerList.ContainsKey(userName)))
+                    {
+                        List<List<string>> info = new List<List<string>>(2);
+                        List<string> followers = new List<string>();
+                        List<string> following = new List<string>();
+                        info.Add(followers);
+                        info.Add(following);
+
+                        vLoggerList.Add(userName,info);
+                    }
+                }
+                else if (command == "followed")
+                {
+                    string followedUser = input[2];
+
+                    if (vLoggerList.ContainsKey(userName) && vLoggerList.ContainsKey(followedUser) && userName != followedUser)
+                    {
+                        if (!vLoggerList[userName][1].Contains(followedUser))
+                        {
+                            vLoggerList[userName][1].Add(followedUser);
+                            vLoggerList[followedUser][0].Add(userName);
+                        }
+                    }
+                }
+
+                input = Console.ReadLine().Split().ToArray();
+            }
+
+            var orderedvLoggerList = vLoggerList.OrderByDescending(x => x.Value[0].Count()).ThenBy(x=> x.Value[1].Count);
+
+            Console.WriteLine($"The V-Logger has a total of {orderedvLoggerList.Count()} vloggers in its logs.");
+            int count = 1;
+            var firsVlogger = orderedvLoggerList.First();
+
+            Print(count, firsVlogger.Key, firsVlogger.Value[0].Count, firsVlogger.Value[1].Count);
+
+            foreach (var v in firsVlogger.Value[0].OrderBy(x=>x))
+            {
+                Console.WriteLine($"*  {v}");
+            }
+
+            foreach (var v in orderedvLoggerList)
+            {
+                if (count == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    Print(count, v.Key, v.Value[0].Count, v.Value[1].Count);
+                    count++;
+                }
+            }
+        }
+
+        private static void Print(int count, string name, int followers, int following)
+        {
+            Console.WriteLine($"{count}. {name} : {followers} followers, {following} following");
+        }
+    }
+}
---
+++ C#-Advanced/Sets-And-Dictionaries/Exercise/Sets-And-Dictionaries-Exercise/08-Ranking/Ranking.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _08_Ranking
+{
+    class Ranking
+    {
+        static void Main(string[] args)
+        {
+            string[] inputContests = Console.ReadLine().Split(":").ToArray();
+            Dictionary<string, string> contests = new Dictionary<string, string>();
+
+            while (inputContests[0] != "end of contests")
+            {
+                string contest = inputContests[0];
+                string pass = inputContests[1];
+
+                contests.Add(contest,pass);
+
+                inputContests = Console.ReadLine().Split(":").ToArray();
+            }
+
+            string[] inputUsers = Console.ReadLine().Split("=>").ToArray();
+            SortedDictionary<string, Dictionary<string, int>> usersInfo = new SortedDictionary<string, Dictionary<string, int>>();
+
+            while (inputUsers[0] != "end of submissions")
+            {
+                string contest = inputUsers[0];
+                string pass = inputUsers[1];
+                string user = inputUsers[2];
+                int scores = int.Parse(inputUsers[3]);
+
+                if (contests.ContainsKey(contest) && contests[contest] == pass)
+                {
+                    if (usersInfo.ContainsKey(user))
+                    {
+                        if (usersInfo[user].ContainsKey(contest))
+                        {
+                            if (usersInfo[user][contest] < scores)
+                            {
+                                usersInfo[user][contest] = scores;
+                            }
+                        }
+                        else
+                        {
+                            usersInfo[user].Add(contest, scores);
+                        }
+                    }
+                    else
+                    {
+                        Dictionary<string, int> currUserContests = new Dictionary<string, int>();
+                        currUserContests.Add(contest,scores);
+                        usersInfo.Add(user, currUserContests);
+                    }
+                }
+
+                inputUsers = Console.ReadLine().Split("=>").ToArray();
+            }
+
+            var maxUser = usersInfo.OrderByDescending(x => x.Value.Sum(s => s.Value)).First();
+
+            Console.WriteLine($"Best candidate is {maxUser.Key} with total {maxUser.Value.Sum(x => x.Value)} points.");
+            Console.WriteLine("Ranking:");
+
+            foreach (var user in usersInfo)
+            {
+                var temp = user.Value.OrderByDescending(x => x.Value);
+                Console.WriteLine(user.Key);
+                foreach (var cont in temp)
+                {
+                    Console.WriteLine($"#  {cont.Key} -> {cont.Value}");
+                }
+            }
+        }
+    }
+}
