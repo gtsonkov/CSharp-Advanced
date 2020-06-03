1a1ae00894ffba21d9217152b5cf9bde1f802bf9
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-03 20:53:35 +0200

    -C# Streams and Files -Lab -01

---
+++ C#-Advanced/Streams-And-Files/Lab/Streams-And-Files-Lab/01-Odd-Lines/OddLines.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace _01_Odd_Lines
+{
+    class OddLines
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine("Data", "input.txt");
+            string dest = Path.Combine("Data", "output.txt");
+
+            using (FileStream file = new FileStream(path, FileMode.Open))
+            {
+                using (TextReader textReader = new StreamReader(file))
+                {
+                    using (FileStream destFile = new FileStream(dest, FileMode.Create))
+                    {
+                        using (TextWriter textWriter = new StreamWriter(destFile))
+                        {
+                            string line = textReader.ReadLine();
+                            int count = 0;
+                            while (line != null)
+                            {
+                                if (count % 2 != 0)
+                                {
+                                    textWriter.WriteLine(line);
+                                }
+
+                                count++;
+
+                                line = textReader.ReadLine();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
