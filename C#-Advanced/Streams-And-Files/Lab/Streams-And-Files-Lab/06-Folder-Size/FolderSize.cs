02afa79170550042b40ce7af15893a67c028d7d7
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-04 19:01:14 +0200

    -C# Streams and Files -Lab -06

---
+++ C#-Advanced/Streams-And-Files/Lab/Streams-And-Files-Lab/06-Folder-Size/FolderSize.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace _06_Folder_Size
+{
+    class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            //string dataPath = Path.Combine("C:", "Microsoft");
+            string dataPath = Path.Combine("SomeDir");
+
+            var files = Directory.GetFiles(dataPath);
+
+            double size = GetSize(dataPath);
+
+            Console.WriteLine($"Directory size: {size} Byte");
+        }
+
+        private static long GetSize(string path)
+        {
+            var directory = Directory.GetDirectories(path);
+
+            if (directory.Length > 0)
+            {
+                long size = 0;
+
+                foreach (var dir in directory)
+                {
+
+                    var files = Directory.GetFiles(dir);
+
+                    var u_directory = Directory.GetDirectories(Path.GetFullPath(dir));
+
+                    if (u_directory.Length>0)
+                    {
+                        size += GetSize(Path.GetFullPath(dir));
+
+                        foreach (var file in files)
+                        {
+                            var info = new FileInfo(file);
+                            size += info.Length;
+                        }
+                    }
+                    else
+                    {
+                        foreach (var file in files)
+                        {
+                            var info = new FileInfo(file);
+                            size += info.Length;
+                        }
+                    }
+                }
+
+                return size;
+            }
+
+            else
+            {
+                long size = 0;
+            
+                var files = Directory.GetFiles(path);
+            
+                foreach (var file in files)
+                {
+                    var info = new FileInfo(file);
+                    size += info.Length;
+                }
+                
+                return size;
+            }
+            
+        }
+    }
+}
