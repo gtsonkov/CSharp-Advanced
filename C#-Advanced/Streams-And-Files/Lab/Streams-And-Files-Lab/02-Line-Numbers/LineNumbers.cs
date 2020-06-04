e6ae7c8238cbec53e7038306eae4468ccf8ff740
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-04 16:40:23 +0200

    -C# Streams and Files -Lab -02

---
+++ C#-Advanced/Streams-And-Files/Lab/Streams-And-Files-Lab/02-Line-Numbers/LineNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _02_Line_Numbers
+{
+    class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine("Data","input.txt");
+            string dest = Path.Combine("Data", "output.txt");
+
+            
+
+            var lines = File.ReadAllLines(path);
+
+            string[] sb = new string[lines.Length];
+
+            int counter = 1;
+
+            foreach (var line in lines)
+            {
+                sb[counter-1] = ($"{counter}. " + line);
+                counter++;
+            }
+
+            File.WriteAllLines(dest, sb);
+        }
+    }
+}
