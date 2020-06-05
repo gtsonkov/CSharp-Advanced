1cc05740712aae61e3b6841944dc71572553ea29
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-05 21:23:52 +0200

    -C# Streams and Files -Lab -05

---
+++ C#-Advanced/Streams-And-Files/Lab/Streams-And-Files-Lab/05-File-Slice/FileSlice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _05_File_Slice
+{
+    class FileSlice
+    {
+        static void Main(string[] args)
+        {
+            string textPath = Path.Combine("Data", "input.txt");
+
+            using (var input = new FileStream(textPath,FileMode.Open))
+            {
+                long size = input.Length;
+
+                long partSize = (long)Math.Ceiling((double)size/4);
+
+                long lastReadStart = 0;
+
+                for (int i = 1; i <= 4; i++)
+                {
+                    byte[] buffer = new byte[partSize];
+
+                    using (var output = new FileStream($"Data\\Part{i}.txt",FileMode.Create))
+                    {
+                        int bytesReaded = input.Read(buffer, 0, (int)partSize);
+                        lastReadStart += partSize;
+                        output.Write(buffer,0, bytesReaded);
+                    }
+                }
+            }
+        }
+    }
+}
