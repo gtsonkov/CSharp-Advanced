86f88be02987c7a6772b66b5a4efe58a3430d646
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-06 23:00:24 +0200

    -additionally exercise - Time test

---
+++ C#-Advanced/Streams-And-Files/Lab/Streams-And-Files-Lab/00-TimeTest-LineNumbers/TimeTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace _00_TimeTest_LineNumbers
+{
+    class TimeTest
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine("Data", "input.txt");
+            string dest = Path.Combine("Data", "output.txt");
+
+            var timer = new Stopwatch();
+
+            timer.Start();
+
+            var lines = File.ReadAllLines(path);
+
+            string[] sb = new string[lines.Length];
+
+            int counter = 1;
+
+            foreach (var line in lines)
+            {
+                sb[counter - 1] = ($"{counter}. " + line);
+                counter++;
+            }
+
+            File.WriteAllLines(dest, sb);
+
+            Console.WriteLine(timer.Elapsed);
+        }
+    }
+}
