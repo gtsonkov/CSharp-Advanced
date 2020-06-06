c7c0b60a871508542b8af96a9a6e42fe73cc93d9
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-07 01:25:37 +0200

    -Streams and files -exercise -05, -06

---
+++ C#-Advanced/Streams-And-Files/Exercise/Streams-And-Files-Exercise/05-Directory-Traversal/DirektoryTraversal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _05_Directory_Traversal
+{
+    class DirektoryTraversal
+    {
+        static void Main(string[] args)
+        {
+            string searchedExtension = ".";
+            string dirPath = Environment.CurrentDirectory;
+            string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string writePath = Path.Combine(desktopFolder, "report.txt");
+
+            StringBuilder sb = new StringBuilder();
+
+            var files = Directory.GetFiles(dirPath, $"{searchedExtension}");
+
+            Dictionary<string, Dictionary<string, double>> data = new Dictionary<string, Dictionary<string, double>>();
+
+            foreach (var f in files)
+            {
+                FileInfo info = new FileInfo(f);
+
+                string extension = info.Extension;
+                string fileName = info.Name;
+                double size = (info.Length / 1024.00);
+
+                if (!data.ContainsKey(extension))
+                {
+                    data.Add(extension, new Dictionary<string, double>());
+                }
+
+                data[extension].Add(fileName, size);
+            }
+                foreach (var ex in data
+                    .OrderByDescending(x => x.Value.Count())
+                    .ThenBy(x => x.Key))
+                {
+                    sb.AppendLine(ex.Key);
+
+                    foreach (var file in ex.Value.OrderBy(x => x.Value))
+                    {
+                        sb.AppendLine($"--{file.Key}{ex.Key} - {file.Value:f3}kb");
+                    }
+                }
+                
+            
+            File.WriteAllText(writePath, sb.ToString());
+        }
+    }
+}
---
+++ C#-Advanced/Streams-And-Files/Exercise/Streams-And-Files-Exercise/06-Zip-And-Extract/ZipAndExtract.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace _06_Zip_And_Extract
+{
+    class ZipAndExtract
+    {
+        static void Main(string[] args)
+        {
+            string pathFileToZip = Path.Combine("Data");
+            string pathToSave = "../../myZip.zip";
+            string pathToUnzip = "../../../ myZip.zip";
+            ZipFile.CreateFromDirectory(pathFileToZip, pathToSave);
+            ZipFile.ExtractToDirectory(pathToSave,pathToUnzip);
+        }
+    }
+}
