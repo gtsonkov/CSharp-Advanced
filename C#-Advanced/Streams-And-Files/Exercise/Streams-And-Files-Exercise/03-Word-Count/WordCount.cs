362e55f8de2b00ce34f4853cacc5952f3551a283
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-06 22:09:55 +0200

    -Streams and files -exercise -03

---
+++ C#-Advanced/Streams-And-Files/Exercise/Streams-And-Files-Exercise/03-Word-Count/WordCount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _03_Word_Count
+{
+    class WordCount
+    {
+        static void Main(string[] args)
+        {
+            string textPath = Path.Combine("Data", "text.txt");
+            string wordsPath = Path.Combine("Data", "words.txt");
+            string destPath = Path.Combine("Data", "output.txt");
+
+            var text = File.ReadAllText(textPath).Split(new[] { '.', ' ', ',', '-', '?', '!', ':', ';' },
+                    StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            var wordsToSearch = File.ReadAllText(wordsPath).Split(Environment.NewLine).ToArray();
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            foreach (var w in wordsToSearch)
+            {
+                words.Add(w, 0);
+            }
+
+            foreach (var t in wordsToSearch)
+            {
+                foreach (var w in text)
+                {
+                    if (w.ToLower() == (t.ToLower()))
+                    {
+                        words[t]++;
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var w in words.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine(w.Key + "-" + w.Value);
+                sb.AppendLine(w.Key + "-" + w.Value);
+            }
+
+            File.WriteAllText(destPath, sb.ToString());
+        }
+    }
+}
