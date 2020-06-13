f98d6e1c01e844520fd453d9b87895da0ff36f82
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-13 23:34:17 +0200

    -init Functional Programming -Lab

---
+++ C#-Advanced/Functional-Programming/Functional-Programming-Lab/01-Sort-Event-Numbers/SortEventNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_Sort_Event_Numbers
+{
+    class SortEventNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x => (x % 2) == 0)
+                .OrderBy(x => x)
+                .ToArray()));
+        }
+    }
+}
