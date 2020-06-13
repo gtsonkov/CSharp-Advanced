317e3c9d70dc012c83c5d09628e58af47f7d4ee7
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-13 23:47:42 +0200

    -Edit Order names

---
+++ C#-Advanced/05-Functional-Programming/Lab/Functional-Programming-Lab/02-Sum-Numbers/SumNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_Sum_Numbers
+{
+    class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(input.Length);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
