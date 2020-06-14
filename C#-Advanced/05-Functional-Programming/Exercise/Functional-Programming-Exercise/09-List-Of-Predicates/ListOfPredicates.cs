5310a94c25b485dc9d1577afa7b87947cdac04db
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-14 22:47:51 +0200

    -init Functional Programming -Exercise -09, -10

---
+++ C#-Advanced/05-Functional-Programming/Exercise/Functional-Programming-Exercise/09-List-Of-Predicates/ListOfPredicates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_List_Of_Predicates
+{
+    class ListOfPredicates
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> range = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                range.Add(i);
+            }
+
+            foreach (var num in range)
+            {
+                bool disible = true;
+
+                foreach (var divider in dividers)
+                {
+                    if ((num % divider) != 0)
+                    {
+                        disible = false;
+                        break;
+                    }
+                }
+                if (disible)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
---
+++ C#-Advanced/05-Functional-Programming/Exercise/Functional-Programming-Exercise/10-Predicate-Party/PredicateParty.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10_Predicate_Party
+{
+    class PredicateParty
+    {
+        static void Main(string[] args)
+        {
+            var listOfPeople = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            string[] command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            while (command[0] != "Party!")
+            {
+                string executeCommand = command[0];
+                string action = command[1];
+                string criterial = command[2];
+
+                Func<string, bool> startWith = x => x.StartsWith(criterial);
+                Func<string, bool> endWith = x => x.EndsWith(criterial);
+
+
+                switch (executeCommand)
+                {
+                    case "Double":
+
+                        var listOfFilteredPeople = new List<string>();
+                        switch (action)
+                        {
+                            case "StartsWith":
+                                listOfFilteredPeople = listOfPeople.Where(startWith).ToList();
+                                break;
+
+                            case "EndsWith":
+                                listOfFilteredPeople = listOfPeople.Where(endWith).ToList();
+                                break;
+
+                            case "Length":
+                                listOfFilteredPeople = listOfPeople.Where(x => x.Length == int.Parse(criterial)).ToList();
+                                break;
+                        }
+
+                        listOfPeople.InsertRange(0,listOfFilteredPeople);
+                        break;
+
+                    case "Remove":
+                        {
+                            var listOfFilteredPeople2 = new List<string>();
+
+                            switch (action)
+                            {
+                                case "StartsWith":
+
+                                    listOfFilteredPeople2 = listOfPeople.Where(startWith).ToList();
+
+                                    Action<List<string>> removePeopleStart = peoplecoll =>
+                                    {
+                                        for (int i = 0; i < listOfFilteredPeople2.Count; i++)
+                                        {
+                                            listOfPeople.Remove(listOfFilteredPeople2[i]);
+                                        }
+                                    };
+                                    removePeopleStart(listOfFilteredPeople2);
+
+                                    break;
+
+                                case "EndsWith":
+                                    listOfFilteredPeople2 = listOfPeople.Where(endWith).ToList();
+
+                                    Action<List<string>> removePeopleEnd = peoplecoll =>
+                                    {
+                                        for (int i = 0; i < listOfFilteredPeople2.Count; i++)
+                                        {
+                                            listOfPeople.Remove(listOfFilteredPeople2[i]);
+                                        }
+                                    };
+
+                                    removePeopleEnd(listOfFilteredPeople2);
+                                    break;
+
+                                case "Length":
+                                    listOfFilteredPeople2 = listOfPeople.Where(x => x.Length == int.Parse(criterial)).ToList();
+
+                                    Action<List<string>> removePeopleLenght = peoplecoll =>
+                                    {
+                                        for (int i = 0; i < listOfFilteredPeople2.Count; i++)
+                                        {
+                                            listOfPeople.Remove(listOfFilteredPeople2[i]);
+                                        }
+                                    };
+                                    removePeopleLenght(listOfFilteredPeople2);
+                                    break;
+                            }
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+
+            if (listOfPeople.Count > 0)
+            {
+                Console.WriteLine($"{string.Join(", ", listOfPeople)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+    }
+}
