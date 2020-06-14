b69dfb05953160f7252acea767a5419bc9353542
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-14 19:41:29 +0200

    -init Functional Programming -Exercise -12, -11

---
+++ C#-Advanced/05-Functional-Programming/Exercise/Functional-Programming-Exercise/11-Party-Reservation-Filter-Module/PartyFilter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11_Party_Reservation_Filter_Module
+{
+    class PartyFilter
+    {
+        private static Func<string, bool> ApllyFilter(string filterType, string condition)
+        {
+            switch (filterType)
+            {
+                case "Starts with":
+                    return (x => x.StartsWith(condition));
+
+                case "Ends with":
+                    return (x => x.EndsWith(condition));
+
+                case "Lenght":
+                    return (x => x.Length == int.Parse(condition));
+
+                case "Contains":
+                    return (x => x.Contains(condition));
+
+                default: return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> peopleList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var command = Console.ReadLine()
+                .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                      .ToArray();
+
+            List<string> commands = new List<string>();
+
+            while (command[0] != "Print")
+            {
+                if (command[0].Contains("Add"))
+                {
+                    commands.Add($"{command[1]}, {command[2]}");
+                }
+                else
+                {
+                    commands.Remove($"{command[1]}, {command[2]}");
+                }
+
+                command = Console.ReadLine()
+                      .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                      .ToArray();
+            }
+            var filteredData = new List<string>();
+            
+            foreach (var cm in commands)
+            {
+                string commandType = cm.Split(", ").ToArray()[0];
+                string token = cm.Split(", ").ToArray()[1];
+                var currFilter = ApllyFilter(commandType, token);
+
+                peopleList = peopleList.Where((p => !currFilter(p))).ToList();
+            }
+
+            Console.WriteLine(string.Join(" ", peopleList));
+        }
+    }
+}
---
+++ C#-Advanced/05-Functional-Programming/Exercise/Functional-Programming-Exercise/12-TriFunction/TriFunktion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _12_TriFunction
+{
+    class TriFunktion
+    {
+        static void Main(string[] args)
+        {
+            int targetSum = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Func<string, int> GetCharAsciiValueSum = sav => sav.Select(c => (int)c).Sum();
+
+            Console.WriteLine(names
+                .Where(n => GetCharAsciiValueSum(n) >= targetSum)
+                .FirstOrDefault());
+        }
+    }
+}
