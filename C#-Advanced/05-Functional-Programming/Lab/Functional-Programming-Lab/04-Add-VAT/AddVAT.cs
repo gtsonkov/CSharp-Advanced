bedeb2908876ec4cfcb645d41d701b4989c9ca43
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-14 17:34:37 +0200

    Functional Programming -Lab -04, -05

---
+++ C#-Advanced/05-Functional-Programming/Lab/Functional-Programming-Lab/04-Add-VAT/AddVAT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_Add_VAT
+{
+    class AddVAT
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x =>
+                {
+                    double num = double.Parse(x);
+                    return num * 1.2;
+                })
+                .ToList()
+                .ForEach(s => Console.WriteLine($"{s:F2}"));
+        }
+    }
+}
---
+++ C#-Advanced/05-Functional-Programming/Lab/Functional-Programming-Lab/05-Filter-By-Age/FilterByAge.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Filter_By_Age
+{
+    class FilterByAge
+    {
+        public static Func<int, bool> CreateTester(string condition, int age)
+        {
+            switch (condition)
+            {
+                case "younger":
+                    return x => x < age;
+                
+                case "older":
+                    return x => x >= age;
+
+                default: return null; 
+            }
+        }
+
+        public static Action<KeyValuePair<string,int>> CreatePrinter(string format)
+        {
+            switch (format)
+            {
+                case "name": return p => Console.WriteLine($"{p.Key}");
+
+                case "name age": return p => Console.WriteLine($"{p.Key} - {p.Value}");
+
+                default: return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            var data = new Dictionary<string, int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(", ")
+                    .ToArray();
+                data.Add(input[0], int.Parse(input[1]));
+            }
+            string condition = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string outputFormat = Console.ReadLine();
+            var getDataByCondition = CreateTester(condition,age);
+
+            var printer = CreatePrinter(outputFormat);
+
+            var filteredData = data.Where(x => getDataByCondition(x.Value));
+
+            foreach (var p in filteredData)
+            {
+                printer(p);
+            }
+
+        }
+    }
+}
