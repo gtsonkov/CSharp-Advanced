baedde6cb5ba1f74cf3eec338a7973d442993ddd
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-14 15:56:42 +0200

    Functional Programming -Lab 03

--- C#-Advanced/05-Functional-Programming/Lab/Functional-Programming-Lab/02-Sum-Numbers/SumNumbers.cs
+++ C#-Advanced/05-Functional-Programming/Lab/Functional-Programming-Lab/02-Sum-Numbers/SumNumbers.cs
@@ -7,9 +7,23 @@ namespace _02_Sum_Numbers
     {
         static void Main(string[] args)
         {
+            //Variant 1
+
+            //int[] input = Console.ReadLine()
+            //    .Split(", ")
+            //    .Select(int.Parse)
+            //    .ToArray();
+            //Console.WriteLine(input.Length);
+            //Console.WriteLine(input.Sum());
+
+            //Variant 2
             int[] input = Console.ReadLine()
                 .Split(", ")
-                .Select(int.Parse)
+                .Select(e =>
+                {
+                    int result = int.Parse(e);
+                    return result;
+                })
                 .ToArray();
             Console.WriteLine(input.Length);
             Console.WriteLine(input.Sum());
---
+++ C#-Advanced/05-Functional-Programming/Lab/Functional-Programming-Lab/03-Count-Uppercase-Words/CountUppercaseWords.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Count_Uppercase_Words
+{
+    class CountUppercaseWords
+    {
+        static void Main(string[] args)
+        {
+            #region Variant 1
+
+            Console.WriteLine(string.Join(Environment.NewLine, Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Where(e => char.IsUpper(e[0]))
+                .ToArray()));
+
+            #endregion
+
+            #region Variant 2
+
+            //string[] inpit = Console.ReadLine()
+            //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //    .ToArray();
+            //
+            //foreach (var item in inpit)
+            //{
+            //    if (char.IsUpper(item[0]))
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion
+
+            #region Variant 3
+            //Predicate<string> UppercaseChecker = 
+            //    str => char.IsUpper(str[0]);
+            //Console.ReadLine()
+            //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //    .Where(w => UppercaseChecker(w))
+            //    .ToList()
+            //    .ForEach(w => Console.WriteLine(w));
+            #endregion
+        }
+    }
+}
