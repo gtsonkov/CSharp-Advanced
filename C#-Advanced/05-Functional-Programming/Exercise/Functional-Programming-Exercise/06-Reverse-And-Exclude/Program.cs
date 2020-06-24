a496fff5a637ea673b63cc23ba30a6c4cee4b3ef
Georgi Tsonkov <gtsonkov@abv.bg>
2020-06-24 21:43:30 +0200

    -Fuctional-Programming-Exercise -06, 07

---
+++ C#-Advanced/05-Functional-Programming/Exercise/Functional-Programming-Exercise/06-Reverse-And-Exclude/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            var result = input.Where(x => x % n != 0).Reverse();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ C#-Advanced/05-Functional-Programming/Exercise/Functional-Programming-Exercise/07-Predicate-For-Names/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLenght = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine().Split(" ").Where(x => x.Length <= maxLenght).ToList();
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
