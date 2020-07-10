f6964fccf9192b63b95afe05cb132a805b1aec70
Georgi Tsonkov <gtsonkov@abv.bg>
2020-07-10 22:30:45 +0200

    OOP- Inheritance -Exercise

---
+++ C#-OOP/01-Inheritance/Exercise/Person/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace Person
+{
+    public class Person
+    {
+        private string _name;
+
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name 
+        { 
+            get { return this._name; } 
+            private set { this._name = value; } 
+        }
+
+        public int Age
+        {
+            get { return this._age; }
+            private set { this._age = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
--- C#-OOP/01-Inheritance/Exercise/Person/StartUp.cs
+++ C#-OOP/01-Inheritance/Exercise/Person/StartUp.cs
@@ -1,10 +1,17 @@
-﻿namespace Person
+﻿using System;
+
+namespace Person
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
 
+            Child newChild = new Child(name, age);
+
+            Console.WriteLine(newChild);
         }
     }
 }
