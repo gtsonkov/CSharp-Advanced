fd79ad43c0b961979f80a8563f54c59214e8eaa7
Georgi Tsonkov <gtsonkov@abv.bg>
2020-07-10 20:41:32 +0200

    OOP- Inheritance -Lab

---
+++ C#-OOP/01-Inheritance/Lab/Inheritance-Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    /// <summary>
+    /// Remove random Element from List
+    /// </summary>
+    public class RandomList : List<string>
+    {
+        private Random _random;
+
+        public RandomList(IEnumerable<string> data)
+            :base (data)
+        {
+            this._random = new Random();
+        }
+
+        public string RandomString()
+        {
+            string result = string.Empty;
+
+            int index = _random.Next(0, this.Count);
+
+            result = this[index];
+
+            this.RemoveAt(index);
+
+            return result;
+        }
+    }
+}
---
+++ C#-OOP/01-Inheritance/Lab/Inheritance-Lab/CustomRandomList/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList myRandomList = new RandomList(new string[] { "A", "B", "C", "D", "E", "F", "G", });
+
+            Console.WriteLine(myRandomList.RandomString());
+        }
+    }
+}
