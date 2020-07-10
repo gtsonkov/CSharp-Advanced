4ba0b6975e292d32ccc8949017382e482585452d
Georgi Tsonkov <gtsonkov@abv.bg>
2020-07-10 21:15:03 +0200

    OOP- Inheritance -Lab

---
+++ C#-OOP/01-Inheritance/Lab/Inheritance-Lab/StackOfString/StackString.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace StackOfString
+{
+    public class StackString : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
---
+++ C#-OOP/01-Inheritance/Lab/Inheritance-Lab/StackOfString/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StackOfString
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackString myStack = new StackString();
+
+            myStack.Push("A");
+            myStack.Push("B");
+            myStack.Push("C");
+
+            myStack.AddRange(new string[] { "C", "D", "E" });
+
+            while (!myStack.IsEmpty())
+            {
+                Console.WriteLine(myStack.Pop());
+            }
+
+        }
+    }
+}
