bf211111f33bc93086f476b6d7787810ddce5f3c
Georgi Tsonkov <gtsonkov@abv.bg>
2020-07-11 14:11:00 +0200

    OOP- Inheritance -Exercise

---
+++ C#-OOP/01-Inheritance/Exercise/Restaurant/Cake.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double Grams = 250.00;
+        private const double Calories = 1000.00;
+        private const decimal Price = 5.00M;
+
+        public Cake(string name) 
+            : base(name, Price, Grams, Calories)
+        {
+        }
+    }
+}
---
+++ C#-OOP/01-Inheritance/Exercise/Restaurant/Fish.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant
+{
+    public class Fish : MainDish
+    {
+        private const double Grams = 22.0;
+
+        public Fish(string name, decimal price)
+            : base(name, price, Grams)
+        {
+        }
+    }
+}
