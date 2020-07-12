d63f548e0d6e9463b104f3a5a705a31e0ae953b6
Georgi Tsonkov <gtsonkov@abv.bg>
2020-07-12 11:53:16 +0200

    OOP- Encapsulation -Lab

---
+++ C#-OOP/02-Encapsulation/Lab/Encapsulation-Lab/02-Salary-Increase/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public Person(string name, string lastName, int age, decimal salary)
+        {
+            this.FirstName = name;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
