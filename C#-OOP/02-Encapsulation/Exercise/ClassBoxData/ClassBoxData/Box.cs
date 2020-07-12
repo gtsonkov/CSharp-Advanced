935eb49005abb9f1ea93caddf49e8bbc9a640864
Georgi Tsonkov <gtsonkov@abv.bg>
2020-07-12 20:42:37 +0200

    -init -OOP- Encapsulation -Exercise

---
+++ C#-OOP/02-Encapsulation/Exercise/ClassBoxData/ClassBoxData/Box.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+
+        private double width;
+
+        private double height;
+
+        public Box(double l, double w, double h)
+        {
+            this.Length = l;
+            this.Width = w;
+            this.Height = h;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+
+        }
+
+        public double SurfaceArea()
+        {
+            return (2.00*Length*Width + 2.00*Length*Height + 2.00*Width*Height);
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return (2.00*Length*Height + 2.00*Width*Height);
+        }
+
+        public double Volume()
+        {
+            return (Length*Width*Height);
+        }
+
+    }
+}
---
+++ C#-OOP/02-Encapsulation/Exercise/ClassBoxData/ClassBoxData/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+
+            double w = double.Parse(Console.ReadLine());
+
+            double h = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(l, w, h);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.Volume():F2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
