c99b9767c882647bdeb5b66cb50c7d68a1292334
Georgi Tsonkov <gtsonkov@abv.bg>
2020-08-09 19:06:12 +0200

    -C#-00P -ExampPrep-Examp16042020

---
+++ C#-OOP/10-Examp-Preparation/16042020/RobotService/Models/Robots/Robot.cs
@@ -0,0 +1,84 @@
+﻿using RobotService.Models.Robots.Contracts;
+using RobotService.Utilities.Messages;
+using System;
+
+namespace RobotService.Models.Robots
+{
+    public abstract class Robot : IRobot
+    {
+        private const string DefautRobotOwner = "Service";
+        private int happiness;
+        private int energy;
+        private string owner;
+
+        public Robot(string name, int energy, int happiness, int procedureTime)
+        {
+            this.Name = name;
+            this.Energy = energy;
+            this.Happiness = happiness;
+            this.ProcedureTime = procedureTime;
+            this.Owner = DefautRobotOwner;
+        }
+
+        public string Name { get; }
+
+        public int Happiness
+        {
+            get
+            {
+                return this.happiness;
+            }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidHappiness);
+                }
+
+                this.happiness = value;
+            }
+        }
+
+        public int Energy
+        {
+            get
+            {
+                return this.energy;
+            }
+
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidEnergy);
+                }
+
+                this.energy = value;
+
+            }
+        }
+
+        public int ProcedureTime { get; set; }
+
+        public string Owner
+        {
+            get
+            {
+                return this.owner;
+            }
+
+            set
+            {
+                this.owner = value;
+            }
+        }
+
+        public bool IsBought { get ; set ; }
+
+        public bool IsChipped { get ; set ; }
+
+        public bool IsChecked { get ; set; }
+
+        public override string ToString() => $" Robot type: {this.GetType().Name} - {this.Name} - Happiness: {this.Happiness} - Energy: {this.Happiness}";
+    }
+}
