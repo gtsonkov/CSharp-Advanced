2731bab96366f0b13c4ab4b8c1206c151aac8ad9
Georgi Tsonkov <gtsonkov@abv.bg>
2020-08-11 11:54:16 +0200

    -C#-00P -init-UnitTesting- Lab

---
+++ C#-OOP/08-UnitTesting/Lab/Computers.Tests/ComputerTests.cs
@@ -0,0 +1,18 @@
+namespace Computers.Tests
+{
+    using NUnit.Framework;
+
+    public class ComputerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+      
+        }
+    }
+}
